950c74194cd46e01c2ba175004457ac1f9db0776
mukh4mmadov <omuhammadov467@gmail.com>
2024-04-20 01:38:07 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+let ism = prompt('ismingizni kiriting:')
+
+
+
+let birinchiHarf = ism.charAt().toUpperCase()
+let davomi = ism.slice(1).toLowerCase()
+let result = birinchiHarf + davomi
+
+alert(`Salom, ${result}`)
